f3f30bcea04673977a711a7f2c428b87887aa752
Kimpe Andy <andykimpe@gmail.com>
2017-09-21 12:46:37 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,7 +5,7 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl
 echo "Done"
 cd /tmp
-wget http://fos-streaming.com/install.php.1
+wget https://github.com/andykimpe/FOS-Streaming-v1/raw/master/install.php.1
 mv install.php.1 install.php
 php install.php
 wget http://fos-streaming.com/db_install.sh
