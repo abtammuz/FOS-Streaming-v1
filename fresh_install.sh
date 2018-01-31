8df63d94e067cf0d7ab6298a82e80c5be6462cb8
abtammuz <abtammuz@hotmail.com>
2018-01-31 15:53:12 -0600

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,9 +5,9 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl
 echo "Done"
 cd /tmp
-wget https://github.com/andykimpe/FOS-Streaming-v1/raw/master/install.php.1
+wget https://github.com/abtammuz/FOS-Streaming-v1/raw/master/install.php.1
 mv install.php.1 install.php
 php install.php
-wget https://github.com/andykimpe/FOS-Streaming-v1/raw/master/db_install.sh
+wget https://github.com/abtammuz/FOS-Streaming-v1/raw/master/db_install.sh
 chmod 755 db_install.sh
 ./db_install.sh
